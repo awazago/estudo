5cca8d9b6c7cedb3289bcb2980762cd6898074f6
Adams Zago <awazago@gmail.com>
2022-02-07 16:49:27 -0300

    Produto e associacao m to m

--- src/main/java/com/zago/estudo/EstudoApplication.java
+++ src/main/java/com/zago/estudo/EstudoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.zago.estudo.domain.Categoria;
+import com.zago.estudo.domain.Produto;
 import com.zago.estudo.repositories.CategoriaRepository;
+import com.zago.estudo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class EstudoApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoriaRepository catRepo;
+	@Autowired
+	ProdutoRepository prodRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
@@ -26,7 +30,19 @@ public class EstudoApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		catRepo.saveAll(Arrays.asList(cat1, cat2));
+		prodRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
