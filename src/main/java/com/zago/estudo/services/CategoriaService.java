6e9e6bc66acd7dbddc07b86f12c61fd7ec67a215
Adams Zago <awazago@gmail.com>
2022-02-18 18:20:59 -0300

    Exclusao Categorias

--- src/main/java/com/zago/estudo/services/CategoriaService.java
+++ src/main/java/com/zago/estudo/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.zago.estudo.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.zago.estudo.domain.Categoria;
 import com.zago.estudo.repositories.CategoriaRepository;
+import com.zago.estudo.services.exceptions.DataIntegrityException;
 import com.zago.estudo.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
